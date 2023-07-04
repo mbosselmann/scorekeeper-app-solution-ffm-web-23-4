e957c46dcb47200cde21c26f555a8755d92c3671
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-07-04 12:34:26 +0200

    Add solution

--- components/GameForm/GameForm.test.js
+++ components/GameForm/GameForm.test.js
@@ -8,10 +8,57 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders two input fields and a button", () => {});
+test("renders two input fields and a button", () => {
+  render(<GameForm />);
 
-test("renders a form with the accessible name 'Create a new game'", () => {});
+  const inputs = screen.getAllByRole("textbox");
+  expect(inputs).toHaveLength(2);
 
-test("submits the correct form data when every field is filled out", async () => {});
+  const button = screen.getByRole("button");
+  expect(button).toBeInTheDocument();
+});
 
-test("does not submit form if one input field is left empty", async () => {});
+test("renders a form with the accessible name 'Create a new game'", () => {
+  render(<GameForm />);
+
+  const form = screen.getByRole("form", { name: "Create a new game" });
+  expect(form).toHaveAccessibleName();
+});
+
+test("submits the correct form data when every field is filled out", async () => {
+  const createGame = jest.fn();
+  const user = userEvent.setup();
+
+  render(<GameForm onCreateGame={createGame} />);
+
+  const nameOfGame = screen.getByLabelText("Name of game");
+  const playerNames = screen.getByRole("textbox", { name: /player names/i });
+
+  const button = screen.getByRole("button");
+
+  await user.type(nameOfGame, "Monopoly");
+  await user.type(playerNames, "Jan, Andrea, Mareike");
+
+  await user.click(button);
+
+  expect(createGame).toHaveBeenCalledWith({
+    nameOfGame: "Monopoly",
+    playerNames: ["Jan", "Andrea", "Mareike"],
+  });
+});
+
+test("does not submit form if one input field is left empty", async () => {
+  const createGame = jest.fn();
+  const user = userEvent.setup();
+
+  render(<GameForm onCreateGame={createGame} />);
+
+  const nameOfGame = screen.getByRole("textbox", { name: "Name of game" });
+  const button = screen.getByRole("button");
+
+  await user.type(nameOfGame, "Monopoly");
+
+  await user.click(button);
+
+  expect(createGame).not.toHaveBeenCalled();
+});
--- components/Player/Player.test.js
+++ components/Player/Player.test.js
@@ -2,6 +2,41 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  render(<Player name="Jan" score={5} />);
 
-test("calls callbacks when increasing or decreasing score", async () => {});
+  const player = screen.getByText("Jan");
+  expect(player).toBeInTheDocument();
+
+  const score = screen.getByText(5);
+  expect(score).toBeInTheDocument();
+
+  const buttons = screen.getAllByRole("button");
+  expect(buttons).toHaveLength(2);
+});
+
+test("calls callbacks when increasing or decreasing score", async () => {
+  const user = userEvent.setup();
+
+  const decrease = jest.fn();
+  const increase = jest.fn();
+
+  render(
+    <Player
+      name="Jan"
+      score={5}
+      onDecreasePlayerScore={decrease}
+      onIncreasePlayerScore={increase}
+    />
+  );
+
+  const increaseButton = screen.getByRole("button", { name: "Increase Score" });
+  const decreaseButton = screen.getByRole("button", { name: "Decrease Score" });
+
+  await user.click(increaseButton);
+  await user.click(increaseButton);
+  await user.click(decreaseButton);
+
+  expect(increase).toHaveBeenCalledTimes(2);
+  expect(decrease).toHaveBeenCalledTimes(1);
+});
